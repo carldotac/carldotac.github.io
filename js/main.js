bcdc42458805005d15917a5b7af00a5ad5bc9936
Carl Lieberman <carl@bert.home>
2020-02-05 15:09:36 -0500

    Drop conditional logic from short header bar code

--- js/main.js
+++ js/main.js
@@ -5,67 +5,70 @@ var main = {
   bigImgEl : null,
   numImgs : null,
 
-  init : function() {
-    // Shorten the navbar after scrolling a little bit down
-    $(window).scroll(function() {
-        if ($(".navbar").offset().top > -1) {
-            $(".navbar").addClass("top-nav-short");
-            $(".navbar-custom .avatar-container").fadeOut(500);
-        } else {
-            $(".navbar").removeClass("top-nav-short");
-            $(".navbar-custom .avatar-container").fadeIn(500);
-        }
-    });
+    init : function() {
 
-    // On mobile, hide the avatar when expanding the navbar menu
-    $('#main-navbar').on('show.bs.collapse', function () {
-      $(".navbar").addClass("top-nav-expanded");
-    });
-    $('#main-navbar').on('hidden.bs.collapse', function () {
-      $(".navbar").removeClass("top-nav-expanded");
-    });
+        $(".navbar").addClass("top-nav-short");
+        $(".navbar-custom .avatar-container").fadeOut(500);
+        // // Shorten the navbar after scrolling a little bit down
+        // $(window).scroll(function() {
+        //     if ($(".navbar").offset().top > -1) {
+        //         $(".navbar").addClass("top-nav-short");
+        //         $(".navbar-custom .avatar-container").fadeOut(500);
+        //     } else {
+        //         $(".navbar").removeClass("top-nav-short");
+        //         $(".navbar-custom .avatar-container").fadeIn(500);
+        //     }
+        // });
 
-    // On mobile, when clicking on a multi-level navbar menu, show the child links
-    $('#main-navbar').on("click", ".navlinks-parent", function(e) {
-      var target = e.target;
-      $.each($(".navlinks-parent"), function(key, value) {
-        if (value == target) {
-          $(value).parent().toggleClass("show-children");
-        } else {
-          $(value).parent().removeClass("show-children");
-        }
-      });
-    });
-
-    // Ensure nested navbar menus are not longer than the menu header
-    var menus = $(".navlinks-container");
-    if (menus.length > 0) {
-      var navbar = $("#main-navbar ul");
-      var fakeMenuHtml = "<li class='fake-menu' style='display:none;'><a></a></li>";
-      navbar.append(fakeMenuHtml);
-      var fakeMenu = $(".fake-menu");
-
-      $.each(menus, function(i) {
-        var parent = $(menus[i]).find(".navlinks-parent");
-        var children = $(menus[i]).find(".navlinks-children a");
-        var words = [];
-        $.each(children, function(idx, el) { words = words.concat($(el).text().trim().split(/\s+/)); });
-        var maxwidth = 0;
-        $.each(words, function(id, word) {
-          fakeMenu.html("<a>" + word + "</a>");
-          var width =  fakeMenu.width();
-          if (width > maxwidth) {
-            maxwidth = width;
-          }
+        // On mobile, hide the avatar when expanding the navbar menu
+        $('#main-navbar').on('show.bs.collapse', function () {
+            $(".navbar").addClass("top-nav-expanded");
         });
-        $(menus[i]).css('min-width', maxwidth + 'px')
-      });
+        $('#main-navbar').on('hidden.bs.collapse', function () {
+            $(".navbar").removeClass("top-nav-expanded");
+        });
 
-      fakeMenu.remove();
-    }
+        // On mobile, when clicking on a multi-level navbar menu, show the child links
+        $('#main-navbar').on("click", ".navlinks-parent", function(e) {
+            var target = e.target;
+            $.each($(".navlinks-parent"), function(key, value) {
+                if (value == target) {
+                    $(value).parent().toggleClass("show-children");
+                } else {
+                    $(value).parent().removeClass("show-children");
+                }
+            });
+        });
 
-    // show the big header image
-    main.initImgs();
+        // Ensure nested navbar menus are not longer than the menu header
+        var menus = $(".navlinks-container");
+        if (menus.length > 0) {
+            var navbar = $("#main-navbar ul");
+            var fakeMenuHtml = "<li class='fake-menu' style='display:none;'><a></a></li>";
+            navbar.append(fakeMenuHtml);
+            var fakeMenu = $(".fake-menu");
+
+            $.each(menus, function(i) {
+                var parent = $(menus[i]).find(".navlinks-parent");
+                var children = $(menus[i]).find(".navlinks-children a");
+                var words = [];
+                $.each(children, function(idx, el) { words = words.concat($(el).text().trim().split(/\s+/)); });
+                var maxwidth = 0;
+                $.each(words, function(id, word) {
+                    fakeMenu.html("<a>" + word + "</a>");
+                    var width =  fakeMenu.width();
+                    if (width > maxwidth) {
+                        maxwidth = width;
+                    }
+                });
+                $(menus[i]).css('min-width', maxwidth + 'px')
+            });
+
+            fakeMenu.remove();
+        }
+
+        // show the big header image
+        main.initImgs();
   },
 
   initImgs : function() {
